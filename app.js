1445b8457bddfd6e751af48328ca781c7328df37
Aurélie <nn.aurelie@hotmail.com>
2021-08-04 12:16:30 +0200

    Changed package.json

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.use((err, req, res) => {
   res.render('error');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || '5000';
 httpServer.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
 
 module.exports = app;
